91513ccfe656fd73fa4472c12f96e5f11ea0f5dc
smartinez <smartinezramirez@byupathway.edu>
2025-07-05 16:27:43 -0600

    exercise 1 completed

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What's your last name? ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {lastname}, {name} {lastname}");
     }
 }
